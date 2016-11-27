521095d10736c327dcca69c4920c2e76add393b3
Dario Carella <dario.kar@gmail.com>
2016-11-28 00:53:47 +0100

    Updated example

--- example/src/example.js
+++ example/src/example.js
@@ -5,10 +5,22 @@ var OccamyText = require('react-occamy-text');
 var App = React.createClass({
   render () {
     return (
-      <div>
-        <OccamyText>
-          Macaroni tortellini calamari pasta. Salami broccoli ballerina ballerina zucchini tortellini fresco, pizza tombola pizza mozzarella fritti tortellini paparazzi.
-        </OccamyText>
+      <div className="occamy-text-example">
+        <div className="box">
+          <OccamyText>
+            Abra cadabra flipendo, alabif shazam!
+          </OccamyText>
+        </div>
+        <div className="box">
+          <OccamyText>
+            Macaroni tortellini calamari pasta. Salami broccoli ballerina zucchini fresco, pizza tombola mozzarella fritti tortellini paparazzi.
+          </OccamyText>
+        </div>
+        <div className="box">
+          <OccamyText>
+            Lightsaber nintendo geth pace Stark darth algorithm jedi.
+          </OccamyText>
+        </div>
       </div>
     );
   }
